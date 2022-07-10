4a86f8afadbc686b3367bff02223e2999fe7ca8c
Alex Sander <alex.darienko@gmail.com>
2022-07-10 11:52:55 +0300

    Setup project

---
+++ WarStats/WarStats/Classes/Helpers/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  WarStats
+//
+//  Created by Alex Sander on 10.07.2022.
+//
+
+import Foundation
+
+class Constants {
+    
+}
+
+/*
+ https://raw.githubusercontent.com/MacPaw/2022-Ukraine-Russia-War-Dataset/918debb3b328b74173d50cc9824507ed59e9c3c2/data/russia_losses_equipment.json
+ https://raw.githubusercontent.com/MacPaw/2022-Ukraine-Russia-War-Dataset/918debb3b328b74173d50cc9824507ed59e9c3c2/data/russia_losses_personnel.json
+ */
