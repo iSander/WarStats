5cd96f240bb1ae221d0a4c2216a512a0eb565d93
Alex Sander <alex.darienko@gmail.com>
2022-07-10 10:29:50 +0300

    Create  Xcode project

---
+++ WarStats/WarStats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WarStats
+//
+//  Created by Alex Sander on 10.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
